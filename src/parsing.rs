59a4792a246e8d2187aa3e6df0d6866e30ddf788
David L. Inman <davidlevere@gmail.com>
2022-12-22 19:14:11 -0500

    minor refactor to have parse colons return the token tree with the colon in it

--- src/parsing.rs
+++ src/parsing.rs
@@ -3,18 +3,20 @@ use proc_macro::*;
 
 use crate::data::*;
 
-pub fn parse_colon<'a>( input : Input<'a> ) -> Result<((), Input<'a>), Error> {
+pub fn parse_colon<'a>( input : Input<'a> ) -> Result<(&'a TokenTree, Input<'a>), Error> {
     match input.input() { 
-        [TokenTree::Punct(p), rest @ ..] if p.as_char() == ':' => Ok(((), Input::new(rest, p.span()))),
+        [t @ TokenTree::Punct(p), rest @ ..] if p.as_char() == ':' => Ok((t, Input::new(rest, p.span()))),
         [x, ..] => Err(Error::new(x.span(), "expected ':'".to_owned())),
         [] => input.end_of_stream(), 
     }
 }
 
-pub fn parse_colon_colon<'a>( input : Input<'a> ) -> Result<((), Input<'a>), Error> {
-    let (_, input) = parse_colon(input).map_err(|err| err.agument("'::' is missing first ':'".to_owned()))?;
+pub fn parse_colon_colon<'a>( input : Input<'a> ) -> Result<(Vec<&'a TokenTree>, Input<'a>), Error> {
+    let (colon, input) = parse_colon(input).map_err(|err| err.agument("'::' is missing first ':'".to_owned()))?;
     parse_colon(input).map_err(|err| err.agument("'::' is missing second ':'".to_owned()))
+                      .map(|(colon_2, input)| (vec![colon, colon_2], input))
 }
+
 
 pub fn parse_ident<'a>( input : &'a [TokenTree] ) -> Result<(IntraIdent<'a>, &'a [TokenTree]), Span> {
 
